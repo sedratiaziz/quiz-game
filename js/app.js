2a92f5d2eb840185bc23b3f554970036f7496803
sedratiaziz <azizsedrati72@gmail.com>
2024-12-10 22:40:31 +0300

    v1.2.0 Added categories, with each one having threee difficulties: easy, medium well and burnt. also the basic javascript for checking if a choice is clicked is added, in addition to the message displaying.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,46 @@
 /*-------------- Constants -------------*/
 
+const categs = [
+  {
+    categ: "hard",
+    points: 5,
+  },
+  {
+    categ: "medium",
+    points: 3,
+  },
+  {
+    categ: "easy",
+    points: 1,
+  },
+];
 
 /*---------- Variables (state) ---------*/
-
+let score;
+let message;
+let correctChoice;
 
 /*----- Cached Element References  -----*/
-
-
+const question = document.querySelector(".questionGeoEasy");
+const choices = document.querySelectorAll(".btn");
 /*-------------- Functions -------------*/
-
+const init = () => {
+  score = 0;
+  message = "";
+  correctChoice = 10;
+};
 
 /*----------- Event Listeners ----------*/
+const handlePlayerChoice = () => {
+  render();
+};
 
+choices.forEach((element) => {
+  element.addEventListener("click", () => {
+    question.textContent = "Message Displayed !";
+
+    if (element.classList.contains("btn")) {
+      console.log("choice is clicked!");
+    }
+  });
+});
