02a4484739dc743ae54171752fcdde66cfaddd21
sedratiaziz <azizsedrati72@gmail.com>
2024-12-17 18:20:06 +0300

    v4.5.6 fix sound not loading on deployed site

--- js/app.js
+++ js/app.js
@@ -534,9 +534,9 @@ const data = {
 
 /*-------------- Constants -------------*/
 const audioFiles = {
-  correct: new Audio('/assets/audio/correct-sound.mp3'),
-  incorrect: new Audio('/assets/audio/incorrect-sound.mp3'),
-  gamestart: new Audio('/assets/audio/game-start.mp3'),
+  correct: new Audio(`${window.location.origin}/assets/audio/correct-sound.mp3`),
+  incorrect: new Audio(`${window.location.origin}/assets/audio/incorrect-sound.mp3`),
+  gamestart: new Audio(`${window.location.origin}/assets/audio/game-start.mp3`),
 };
 
 const onePoint = document.createElement("span");
